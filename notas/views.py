d0706d05607dc785e87da0906fb1ca94c33dc42a
Tiago Cordeiro <tiago@mulhergorila.com>
2018-10-16 00:06:10 -0300

    Configura Views/Urls para produtos

--- notas/views.py
+++ notas/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Product
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'products.html', {'produtos': products})
