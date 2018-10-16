15c2f3b5b84ae8c1952c6360f32b6198e1bf8f9d
Tiago Cordeiro <tiago@mulhergorila.com>
2018-10-16 13:16:57 -0300

    Configura form para Produtos Refatora

--- notas/urls.py
+++ notas/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('products/', views.product_list, name='products_list'),
+    path('products/', views.product_list, name='product_list'),
+    path('product/add/', views.product_create, name='product_create'),
 ]
--- notas/views.py
+++ notas/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from core.models import UserProfile
+from .forms import ProductForm
 from .models import Product
 
 
@@ -14,3 +15,25 @@ def product_list(request):
     products = Product.objects.all()
     return render(request, 'products/list.html', {'produtos': products,
                                                   'usuario': usuario, })
+
+
+@login_required
+def product_create(request):
+    try:
+        usuario = UserProfile.objects.get(user=request.user)
+    except UserProfile.DoesNotExist:
+        usuario = None
+
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            product = form.save(commit=False)
+            product.added_by = request.user
+            product.save()
+            return redirect(produc_list)
+
+    else:
+        form = ProductForm()
+
+    return render(request, 'products/create.html', {'usuario': usuario,
+                                                    'form': form, })
