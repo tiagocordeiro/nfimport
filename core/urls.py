13f0990d4105f0f101eeeeadd51e78cf02cd71fe
Tiago Cordeiro <tiago@mulhergorila.com>
2018-10-18 04:22:34 -0300

    Configura testes para core Remove url para signup Refatora

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('profile/', views.profile, name='profile'),
     path('profile/update/', views.profile_update, name='profile_update'),
-    path('profile/avatar/', views.avatar_update, name='avatar_update'),
-    path('signup/', views.signup, name='signup'),
+    # path('signup/', views.signup, name='signup'),
 ]
