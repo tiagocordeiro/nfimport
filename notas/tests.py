49cfb454176ec0ac88956acfd5890ea2d6da88b0
Tiago Cordeiro <tiago@mulhergorila.com>
2018-10-18 04:41:17 -0300

    Configura Testes para notas/product

--- notas/tests.py
+++ notas/tests.py
@@ -1,3 +1,41 @@
-from django.test import TestCase
+from django.contrib.auth.models import AnonymousUser, User, Group
+from django.test import RequestFactory, TestCase
 
-# Create your tests here.
+from .views import product_list, product_create
+
+
+class NotasViewsTest(TestCase):
+    def setUp(self):
+        # Every test needs access to the request factory.
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(username='jacob', email='jacob@…', password='top_secret')
+        self.group = Group.objects.create(name='Testes')
+        self.group.user_set.add(self.user)
+
+    def test_product_list_anonimo(self):
+        request = self.factory.get('/products')
+        request.user = AnonymousUser()
+
+        response = product_list(request)
+        self.assertEqual(response.status_code, 302)
+
+    def test_product_list_logado(self):
+        request = self.factory.get('/products')
+        request.user = self.user
+
+        response = product_list(request)
+        self.assertEqual(response.status_code, 200)
+
+    def test_product_create_anonimo(self):
+        request = self.factory.get('/products')
+        request.user = AnonymousUser()
+
+        response = product_create(request)
+        self.assertEqual(response.status_code, 302)
+
+    def test_product_create_logado(self):
+        request = self.factory.get('/products')
+        request.user = self.user
+
+        response = product_create(request)
+        self.assertEqual(response.status_code, 200)
