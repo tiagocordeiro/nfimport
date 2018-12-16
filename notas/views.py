793dbac29ad08dd4b4cc1608d3493e8b6a88c0ea
Tiago Cordeiro <tiago@mulhergorila.com>
2018-12-16 16:17:18 -0200

    Fix: Editar nota salvando (passando ID no template)

--- notas/views.py
+++ notas/views.py
@@ -1,7 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.forms.models import inlineformset_factory
-from django.shortcuts import render, redirect, get_object_or_404, get_list_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import CreateView
 
 from core.models import UserProfile
