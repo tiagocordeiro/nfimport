29fe84e7fb4fa13753f06d26c4ed14bff301a4a7
Tiago Cordeiro <tiago@mulhergorila.com>
2018-12-27 19:15:58 -0200

    Melhora testes

--- notas/tests.py
+++ notas/tests.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.models import AnonymousUser, User, Group
 from django.test import RequestFactory, TestCase
+from django.utils import timezone
 
-from .views import product_list, product_create
+from .views import product_list, product_create, nota_list, nota_create, nota_update
+from .models import Nota
 
 
 class NotasViewsTest(TestCase):
@@ -11,6 +13,9 @@ class NotasViewsTest(TestCase):
         self.user = User.objects.create_user(username='jacob', email='jacob@…', password='top_secret')
         self.group = Group.objects.create(name='Testes')
         self.group.user_set.add(self.user)
+
+        # Nota
+        self.nota = Nota.objects.create(description='Nota Teste', date=timezone.now(), dolar_dia=3.33)
 
     def test_product_list_anonimo(self):
         request = self.factory.get('/products')
@@ -38,4 +43,46 @@ class NotasViewsTest(TestCase):
         request.user = self.user
 
         response = product_create(request)
+        self.assertEqual(response.status_code, 200)
+
+    def test_notas_list_anonimo(self):
+        request = self.factory.get('/notas')
+        request.user = AnonymousUser()
+
+        response = nota_list(request)
+        self.assertEqual(response.status_code, 302)
+
+    def test_notas_list_logado(self):
+        request = self.factory.get('/notas')
+        request.user = self.user
+
+        response = nota_list(request)
+        self.assertEqual(response.status_code, 200)
+
+    def test_nota_create_anonimo(self):
+        request = self.factory.get('/nota/add')
+        request.user = AnonymousUser()
+
+        response = nota_create(request)
+        self.assertEqual(response.status_code, 302)
+
+    def test_nota_create_logado(self):
+        request = self.factory.get('/nota/add')
+        request.user = self.user
+
+        response = nota_create(request)
+        self.assertEqual(response.status_code, 200)
+
+    def test_nota_update_anonimo(self):
+        request = self.factory.get('/nota/edit/')
+        request.user = AnonymousUser()
+
+        response = nota_update(request, pk=self.nota.pk)
+        self.assertEqual(response.status_code, 302)
+
+    def test_nota_update_logado(self):
+        request = self.factory.get('/nota/edit/')
+        request.user = self.user
+
+        response = nota_update(request, pk=self.nota.pk)
         self.assertEqual(response.status_code, 200)
