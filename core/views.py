12854bce68e0b14728251edf91a2524dcade4e37
Tiago Cordeiro <tiago@mulhergorila.com>
2021-07-30 12:25:49 -0300

    Update Dependencies and Fix exchange data.
    - Atualiza dependencias
    - Atualiza versão do python
    - Corrige fonte de dados de cotação de moedas
    
    this close #574, close #575

--- core/views.py
+++ core/views.py
@@ -1,8 +1,3 @@
-from datetime import date, timedelta
-
-import pandas as pd
-import quandl
-from decouple import config
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.forms.models import inlineformset_factory
@@ -11,6 +6,7 @@ from django.shortcuts import render, redirect
 from notas.models import Nota
 from notas.models import NotaItens
 from notas.models import Product
+from .facade import get_usd_cny_exchange
 from .forms import ProfileForm
 from .models import UserProfile
 
@@ -33,25 +29,9 @@ def dashboard(request):
             else:
                 pass
 
-    quandl.ApiConfig.api_key = config('QUANDL_KEY')
-    hoje = date.today()
-    periodo = hoje - timedelta(weeks=4)
-    cotacao_moedas = quandl.get(["BUNDESBANK/BBEX3_D_CNY_USD_CA_AC_000",
-                                 "BUNDESBANK/BBEX3_D_BRL_USD_CA_AB_000"],
-                                start_date=periodo.isoformat(), returns="pandas")
-
-    df = pd.DataFrame(cotacao_moedas)
-    df.reset_index(level=0, inplace=True)
-
-    df.fillna(method='ffill', inplace=True)
-    df.fillna(method='bfill', inplace=True)
-
-    cotacao_cny = []
-    cotacao_brl = []
-
-    for cotacao in df.values:
-        cotacao_cny.append({'data': cotacao[0].strftime('%d/%m/%Y'), 'valor': round(cotacao[1], ndigits=2)})
-        cotacao_brl.append({'data': cotacao[0].strftime('%d/%m/%Y'), 'valor': round(cotacao[2], ndigits=2)})
+    cotacao = get_usd_cny_exchange()
+    cotacao_brl = cotacao[0]
+    cotacao_cny = cotacao[1]
 
     last_cny = cotacao_cny[-1]
     last_cny_data = last_cny['data']
